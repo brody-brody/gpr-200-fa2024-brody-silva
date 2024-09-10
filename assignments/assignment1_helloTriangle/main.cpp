c2bb18741601df7af1a13f75058a6584318b688e
brody-brody <brody.silva@mymail.champlain.edu>
2024-09-10 12:43:07 -0400

    - finished both tutorials to have multi color triangle - need to implement time uniforms to animate triangle

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -11,10 +11,34 @@ const int SCREEN_HEIGHT = 720;
 
 float vertices[] =
 {
-   -0.5f, -0.5f, 0.0f,
-	0.5f, -0.5f, 0.0f,
-	0.0f,  0.5f, 0.0f
+   -0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f, // bottom right
+	0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, // bottom right
+	0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f // bottom right
 }; // gpu will read this as 9 floats
+
+const char* vertexShaderSource = 
+"#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"uniform float time;\n"
+"out vec3 ourColor;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos, 1.0);\n"
+"   ourColor = aColor;\n"
+"}\0"; 
+// holding source code for vertex shader and storing it in a const char
+
+const char* fragmentShaderSource = R"(
+		#version 330 core
+		out vec4 FragColor;
+		in vec3 ourColor;
+
+		void main()
+		{
+			FragColor = vec4(ourColor, 1.0);;
+		}
+		)";
 
 int main() {
 	printf("Initializing...");
@@ -41,13 +65,6 @@ int main() {
 
 	// glNamedBufferData(VBO, sizeof(vertices, vertices, GL_STATIC_DRAW)   ---   equivalency to lines 39 and 40
 
-	const char* vertexShaderSource = "#version 330 core\n"
-		"layout (location = 0) in vec3 aPos;\n"
-		"void main()\n"
-		"{\n"
-		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-		"}\0"; // holding source code for vertex shader and storing it in a const char
-
 	unsigned int vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER); // creating shader object, with the type of vertex shader
 
@@ -62,10 +79,60 @@ int main() {
 	if (!success)
 	{
 		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n", infoLog);
+		printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n%s", infoLog);
+	}
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		printf("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n%s", infoLog);
 	}
 
+	// shader program
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
 
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog); 
+		printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n%s", infoLog);
+	}
+
+	glUseProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader); // after youve created and assigned the shaders to the program, you can delete the shaders
+
+	// linking vertex attributes
+
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+
+	glBindVertexArray(VAO);
+	// copy over from vertices array into a buffer
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// set the vertex attributes pointers
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+
+	glUseProgram(shaderProgram);
+	glBindVertexArray(VAO);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 
 	//Render loop
 	while (!glfwWindowShouldClose(window)) {
@@ -73,8 +140,20 @@ int main() {
 		//Clear framebuffer
 		glClearColor(0.75f, 0.5f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT); // clearing color buffer
+
 		//Drawing happens here!
+		// activate program
+		glUseProgram(shaderProgram);
+
+		//update colors based on uniform time
+		int vertexTimeLocation = glGetUniformLocation(shaderProgram, "time");
+
+		//render the triangle
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		glfwSwapBuffers(window); // presenting to window
+		glfwPollEvents();
 	}
 	printf("Shutting down...");
 }
